75657f07e559ae3b9cb5f126e4292e87a8b4a944
Dominic Ortega <dortega37@cnm.edu>
2016-10-11 13:39:18 -0600

    added image

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,20 @@
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>Hello World!</h1>
+
+		<!-- This is the header-->
+		<header>
+			<h1>Hi! This is my page.</h1>
+		</header>
+		<main>
+
+			<!--begin section 1-->
+			<section>
+				<p>The look in your eyes, I recognize it. You used to have it for me. This should be interesting. That might've been one of the shortest assignments in the history of Starfleet. Now, how the hell do we defeat an enemy that knows us better than we know ourselves? Shields up! Rrrrred alert! Is it my imagination, or have tempers become a little frayed on the ship lately? Worf, It's better than music. It's jazz. Some days you get the bear, and some days the bear gets you. A surprise party? Mr. Worf, I hate surprise parties. I would *never* do that to you. When has justice ever been as simple as a rule book? I guess it's better to be lucky than good. Congratulations - you just destroyed the Enterprise. What's a knock-out like you doing in a computer-generated gin joint like this? And blowing into maximum warp speed, you appeared for an instant to be in two places at once. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. Yes, absolutely, I do indeed concur, wholeheartedly! Commander William Riker of the Starship Enterprise. I think you've let your personal feelings cloud your judgement.</p>
+				<img src="img/prince.png" alt="prince">
+			</section>
+			<section></section>
+			<section></section>
+		</main>
 	</body>
 </html>
