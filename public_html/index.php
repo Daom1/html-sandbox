a392930a6e5eb726a5deabde01dfe6f9173f6232
Dominic Ortega <dortega37@cnm.edu>
2016-10-11 22:47:47 -0600

    added CSS

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">
+		<meta charset="UTF-8"/>
+
+		<!-- add CSS file -->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"/>
+
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
 
-		<!-- This is the header-->
+		<!-- this is the header -->
 		<header>
 			<h1>Hi! This is my page.</h1>
 		</header>
+
+		<!-- this is the main page content -->
 		<main>
 
 			<!--begin section 1-->
@@ -17,8 +23,43 @@
 				<p>The look in your eyes, I recognize it. You used to have it for me. This should be interesting. That might've been one of the shortest assignments in the history of Starfleet. Now, how the hell do we defeat an enemy that knows us better than we know ourselves? Shields up! Rrrrred alert! Is it my imagination, or have tempers become a little frayed on the ship lately? Worf, It's better than music. It's jazz. Some days you get the bear, and some days the bear gets you. A surprise party? Mr. Worf, I hate surprise parties. I would *never* do that to you. When has justice ever been as simple as a rule book? I guess it's better to be lucky than good. Congratulations - you just destroyed the Enterprise. What's a knock-out like you doing in a computer-generated gin joint like this? And blowing into maximum warp speed, you appeared for an instant to be in two places at once. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. Yes, absolutely, I do indeed concur, wholeheartedly! Commander William Riker of the Starship Enterprise. I think you've let your personal feelings cloud your judgement.</p>
 				<img src="img/prince.png" alt="prince">
 			</section>
-			<section></section>
-			<section></section>
+
+			<!-- begin section 2 -->
+			<section>
+				<h2>My Favorite Things</h2>
+				<ul>
+					<li>Senator Arlo</li>
+					<li>Star Wars</li>
+					<li>Beer</li>
+					<li>Donuts</li>
+				</ul>
+			</section>
+
+			<!-- begin section 3 -->
+			<section>
+				<table>
+					<tr>
+						<th>Column One</th>
+						<th>Column Two</th>
+					</tr>
+
+					<tr>
+						<td>Cat</td>
+						<td>Dog</td>
+					</tr>
+
+					<tr>
+						<td>Car</td>
+						<td>Bike</td>
+					</tr>
+
+					<tr>
+						<td>Star Wars</td>
+						<td>Wine</td>
+					</tr>
+				</table>
+			</section>
 		</main>
+
 	</body>
 </html>
